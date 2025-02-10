c064c7b5af8d1cd4607b0ee99c17235e2bebbbaf
dorvt <stegmat@meta.ua>
2025-02-10 20:35:34 +0200

    123

---
+++ Lesson_1.py
@@ -0,0 +1,33 @@
+a=2
+b=3
+sum_a_b = a + b
+print("Це a + b", sum_a_b)
+
+user_name = "Ivan\""
+user_age = 23
+user_salary = 10_000_000_000_000.24
+hourly_pric = 123.5658 
+
+
+print(user_name,  user_age, user_salary)
+
+user_age_salary= user_age + user_salary
+
+print(f"age+salary -- {user_age_salary}")
+
+user_age = user_age + 15
+
+if user_age > 35:
+    print("Hello" + "world")
+else:
+    print("Hi bro!")    
+
+a = 5
+b = 3
+
+summ = a + b #Додавання
+diff = a - b #Віднімання
+mult = a * b #Множення
+divd = a / b #Ділення
+
+123
